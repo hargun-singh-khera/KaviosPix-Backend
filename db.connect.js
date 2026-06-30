6b7d35852c2708a88cea892ef32fcf60ee30413b
Hargun Singh Khera <hargunsinghkhera8@gmail.com>
2026-06-30 14:17:40 +0530

    chore: comment out connection log in database initialization

--- db.connect.js
+++ db.connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const initializeDatabase = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGODB_URI);
-        console.log("connection", connection)
+        // console.log("connection", connection)
         if (connection) {
             console.log("MongoDB Connected Successfully")
         }
